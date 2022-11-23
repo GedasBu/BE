2a47115570d9d8f43a6df23f9af398a07af341f1
GedasBu <gedcka@gmail.com>
2022-11-23 12:07:23 +0200

    Parts pagination implemented

--- controllers/partControler.js
+++ controllers/partControler.js
@@ -1,47 +1,66 @@
 const Part = require("../models/partModel");
 
 exports.getAllParts = async (req, res) => {
-  //Build query
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "sort", "limit", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
+  try {
+    //Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-  //Filtering les('<') or more ('>')
+    //Filtering les('<') or more ('>')
 
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-  let query = Part.find(JSON.parse(queryStr));
+    let query = Part.find(JSON.parse(queryStr));
 
-  //Sorting
+    //Sorting
 
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    //default sort
-    // query = query.sort('-createdAt');
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      //default sort
+      // query = query.sort('-createdAt');
+    }
+
+    // Filtering by fields(Selecting fields)
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 20;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numParts = await Part.countDocuments();
+      if (skip >= numParts) throw new Error("No such page");
+    }
+
+    //Execute query
+    const parts = await query;
+
+    res.status(200).json({
+      status: "success",
+      results: parts.length,
+      data: {
+        parts,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err,
+    });
   }
-
-  // Filtering by fields(Selecting fields)
-
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    query = query.select(fields);
-  } else {
-    query = query.select("-__v");
-  }
-
-  //Execute query
-  const parts = await query;
-
-  res.status(200).json({
-    status: "success",
-    results: parts.length,
-    data: {
-      parts,
-    },
-  });
 };
 
 exports.createPart = async (req, res) => {
